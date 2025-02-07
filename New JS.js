551a1873535887ad84e6586af85642c5bfcc3a78
yuliia moroz <julliamoroz1997@gmail.com>
2025-02-07 09:52:42 +0200

    Return to the first step

--- New JS.js
+++ New JS.js
@@ -1,9 +1,1 @@
 console.log("hello");
-console.log("feature1");
-console.log("feature 2");
-console.log("feature3");
-console.log("feature 4");
-console.log("hello from master 1");
-console.log("hello from master 2");
-console.log("hello from master 3");
-console.log("hello from master 4");
